b6fc6d9c6d36de76bdde7283d50e678babec4f90
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-07-10 11:40:24 -0400

    Ex 4 solution

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,22 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+    result = []
+
+    a.sort(reverse = True)
+
+    for num in a:
+        if num > 0:
+            cache[num] = 0
+        else:
+            if num * -1 in cache:
+                cache[num * -1] = num
+    
+    filtered_cache = [(key, cache[key]) for key in cache if cache[key] != 0]
+    
+    for key, value in filtered_cache:
+        result.append(key)
 
     return result
-
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
